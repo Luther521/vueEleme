44472f1b35333a05995ea1b99e9aebf27b0387fc
谭佳林 <784939830@qq.com>
2018-08-16 15:43:46 +0800

    头部

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
          <header>
-        	
+        	 <span>{{title}}</span>
+        	 <a><i>登录</i>|<i>注册</i></a>
          </header>
     </div>
 </template>
@@ -9,7 +10,7 @@
        export default {
 	    data(){
 	        return{
-	           
+	           title:'ele.me'
 	        }
 	    },
 		mounted(){
@@ -36,5 +37,30 @@
 	    top: 0;
 	    width: 100%;
 	    height: 2.8rem;
+	    span{
+	    	left: .4rem;
+		    font-weight: 400;
+		    font-size: 0.9rem;
+		    color: #fff;
+		    width: 2.3rem;
+		    height: .7rem;
+		    position: absolute;
+		    top: 50%;
+		    transform: translateY(-50%);
+	    }
+	    a{
+	    	right: .4rem;
+		    font-weight: 400;
+		    font-size: 0.9rem;
+		    color: #fff;
+		    width: 4.3rem;
+		    height: .7rem;
+		    position: absolute;
+		    top: 50%;
+		    transform: translateY(-50%);
+		    i{
+		    	color: white;
+		    }
+	    }
 	}
 </style>
