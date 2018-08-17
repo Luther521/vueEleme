ac4a420a5703af22dd2f268bcb5704aa5bbe8907
谭佳林 <784939830@qq.com>
2018-08-17 17:28:32 +0800

    登录页面的制作

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,11 +1,80 @@
 <template>
 	<div>
-		 我是登录页面
+		<div class="login">
+			<mt-field label="账号" placeholder="请输入账号"  v-model="username"  :attr="{ maxlength:20 }"></mt-field>
+			<mt-field label="密码" placeholder="请输入密码" type="password"  :attr="{ maxlength: 10 }"></mt-field>
+			<mt-field label="验证码" >
+			  <img src="" height="45px" width="100px">
+			</mt-field>
+		</div>
+		<div class="btn">
+			<p>温馨提示：未注册过的账号，登录时将自动注册</p>
+			<p>注册过的用户可凭账号密码登录</p>
+			<mt-button type="primary" size="large" @click.native="login">登录</mt-button>
+			<a>重置密码</a>
+		</div>
 	</div>
 </template>
 
 <script>
+	  import { Field,Button } from 'mint-ui';
+       export default {
+	    data(){
+	        return{
+	           username:''
+	        }
+	    },
+		mounted(){
+	        
+	    },
+	    components:{
+          Field,
+          Button
+	    },
+	    computed:{
+	    	
+	        
+	    },
+	    methods:{
+	       checkUser:()=>{
+	       	  if(!this.username){
+	       	  	 alert("11");
+	       	  }
+	       },
+	       login:()=>{
+	          this.checkUser();
+	       }
+	    }
+}
 </script>
-
-<style>
+<style lang="scss" scoped>
+    $cred:red;
+  	.login{
+  		 margin-top: 0.6rem;
+  		a{
+  		 margin-bottom: 0.1rem;
+  		}
+  	}
+  .btn{
+  	 padding-top:0.6rem;
+  	 width: 90%;
+  	 margin: auto ;
+  	 p{
+  	  color:$cred;
+  	  font-size: 0.8rem;
+  	 }
+  	 button{
+  	 	margin-top:0.6rem;
+  	 	position: relative;
+  	 }
+  	 a{
+  	 	float: right;
+	    top: 0.6rem;
+	    display: inline-block;
+	    padding-right: 0.6rem;
+	    color: #4646d6;
+	    font-size: 1.0rem;
+	    padding-top: 0.2rem;
+  	 }
+  }
 </style>
